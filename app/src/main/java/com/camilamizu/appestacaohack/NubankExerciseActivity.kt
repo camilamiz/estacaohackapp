13d08e786bd37d1a71aa454e50e6a1e9c637d76c
Camila Mizumoto <camila.yuri.m@gmail.com>
2020-11-17 22:29:16 -0300

    This is an exercise and this branch will not be merged

---
+++ app/src/main/java/com/camilamizu/appestacaohack/NubankExerciseActivity.kt
@@ -0,0 +1,11 @@
+package com.camilamizu.appestacaohack
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NubankExerciseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nubank_exercise)
+    }
+}
