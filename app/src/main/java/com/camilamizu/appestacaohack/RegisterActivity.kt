27715f927cb5ce6b0ec463d2ecd76c1200c118bf
Camila Mizumoto <camila.yuri.m@gmail.com>
2020-11-21 11:55:47 -0300

    Add spinner logic

--- app/src/main/java/com/camilamizu/appestacaohack/RegisterActivity.kt
+++ app/src/main/java/com/camilamizu/appestacaohack/RegisterActivity.kt
@@ -2,10 +2,25 @@ package com.camilamizu.appestacaohack
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        //Creating list of options for spinner; arrayListOf is immutable so it has to be a val
+        val genderOptions = arrayListOf("Feminino", "Masculino", "Outro")
+
+        //Creating an adapter (layout) for spinner
+        val genderAdapter = ArrayAdapter(
+            this,                                 //Context
+            android.R.layout.simple_spinner_dropdown_item, //Layout
+            genderOptions                                  //List content
+        )
+
+        //Plugging the adapter to spnRegisterGender
+        spnRegisterGender.adapter = genderAdapter
     }
 }
