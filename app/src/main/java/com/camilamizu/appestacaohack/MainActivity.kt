0836c6c6bef0f8726f469bb5615ddb2da46835a3
Camila Mizumoto <camila.yuri.m@gmail.com>
2020-11-21 16:11:57 -0300

    Add logic to log off button

--- app/src/main/java/com/camilamizu/appestacaohack/MainActivity.kt
+++ app/src/main/java/com/camilamizu/appestacaohack/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.camilamizu.appestacaohack
 
 import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -24,5 +27,22 @@ class MainActivity : AppCompatActivity() {
         txvMainFullName.text = "$name $lastName"
         txvMainEmail.text =  email
         txvMainGender.text = gender
+
+        btnMainLogOff.setOnClickListener {
+            //Creating an alert
+            AlertDialog.Builder(this)
+                .setTitle("Atenção!")
+                .setMessage("Deseja mesmo sair?")
+                .setPositiveButton("Sair") { _, _ ->
+                    val mIntent = Intent(this, LoginActivity::class.java)
+                    startActivity(mIntent)
+                    finishAffinity()
+                    Toast.makeText(this, "Usuário deslogado.", Toast.LENGTH_SHORT).show()
+                }
+                .setNeutralButton("Cancelar") { _, _ ->}
+                .setCancelable(false) //If the person clicks out of the alert, it will not be closed
+                .create()
+                .show()
+        }
     }
 }
